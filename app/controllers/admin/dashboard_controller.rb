5c1e4f29ec5768d721a9f726be3a41b6608f452d
Jonas Montonen <jonas.montonen@gmail.com>
2026-05-05 08:52:52 +0200

    Add users section to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < Admin::ApplicationController
   def index
     @unread_messages = Message.unread.count
     @total_messages = Message.count
+    @total_users = User.count
   end
 end
